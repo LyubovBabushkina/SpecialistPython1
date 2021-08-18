18ebb450eef932ac1d8dcbf9d60be9ab5ab0828e
LyubovBabushkina <89097675+LyubovBabushkina@users.noreply.github.com>
2021-08-18 23:36:03 +0300

    Update 04_task_string.py

--- Module3/practice/string/04_task_string.py
+++ Module3/practice/string/04_task_string.py
@@ -4,4 +4,14 @@
 text = "Lorem ipsum dolor sit amet consectetur adipiscing elit Integer porttitor bibendum nisi ut convallis ante"
 # Примечание: для генериации текста можете воспользоваться сайтом: https://ru.lipsum.com/
 
-# TODO: your code here
+list = text.split()
+n = text.rstrip().lstrip().count(" ") + 1
+
+i = 0
+k = 0
+while i < n:
+    strlist = str(list[i])
+    if len(strlist) > 5:
+        k += 1
+    i += 1
+print(k)
