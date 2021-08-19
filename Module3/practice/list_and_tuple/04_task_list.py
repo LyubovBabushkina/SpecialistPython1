54c24f8c887a2b8084233a21cd3736a75290a03d
LyubovBabushkina <89097675+LyubovBabushkina@users.noreply.github.com>
2021-08-19 19:11:54 +0300

    Update 04_task_list.py

--- Module3/practice/list_and_tuple/04_task_list.py
+++ Module3/practice/list_and_tuple/04_task_list.py
@@ -1,4 +1,17 @@
 # Дан список из n элементов, заполненный произвольными целыми числами в диапазоне от -10 до 10.
 # Вывести на экран сумму всех положительных элементов.
 
-# TODO: your code here
+import random
+numbers = []
+n = int(input("n: "))
+i = 0
+while i < n:
+    numbers.append(random.randint(-10, 10))
+    i += 1
+print(numbers)
+
+s = 0
+for el in numbers:
+    if el > 0:
+        s += el
+print("Сумма положит элементов равна ", s)
