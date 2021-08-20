933673a3e4be2ca928da342a234492725630baf0
LyubovBabushkina <89097675+LyubovBabushkina@users.noreply.github.com>
2021-08-21 00:54:15 +0300

    Update 07_task_func.py

--- Module4/practice/funcs_part1/07_task_func.py
+++ Module4/practice/funcs_part1/07_task_func.py
@@ -2,3 +2,18 @@
 # где hh - часы, mm- минуты, ss - секунды.
 # Пример:
 # 29143 секунд → 08:05:43
+
+def time_hms(sec):
+    h = str(sec//3600)
+    if len(h) == 1:
+        h = "0" + h
+    m = str(sec % 3600 // 60)
+    if len(m) == 1:
+        m = "0" + m
+    s = str(sec % 3600 % 60)
+    if len(s) == 1:
+        s = "0" + s
+    return h + ":" + m + ":" + s
+
+sec = 29143
+print(time_hms(sec))
