264e4066dbdfeb5e700a96e6136e01a078811a5e
LyubovBabushkina <89097675+LyubovBabushkina@users.noreply.github.com>
2021-08-22 09:51:37 +0300

    Update 06_hw.py

--- Module3/home_work/06_hw.py
+++ Module3/home_work/06_hw.py
@@ -31,15 +31,41 @@ items = [
         "price": 1700
     },
 ]
-# Найдите:
-print("Товары на складе представлены брэндами: ")
 
-# TODO: your code here
+Brand = []
+for i in range(len(items)):
+    Brand.append(dict(items[i])['brand'])
+# Получили список брэндов с повторами в Brand
+Brand_1 = list(set(Brand))  # список брэндов без повторов
 
-print("На складе больше всего товаров брэнда(ов): ")
+print(f"Товары на складе представлены брэндами:",  ', '.join(Brand_1))
 
-# TODO: your code here
+length = len(Brand_1)  # сколько наименований брендов
+number = []
 
-print("На складе самый дорогой товар брэнда(ов): ")
+for i in range(length):
+    m = Brand.count(Brand_1[i])
+    number.append(m)
+# number - список количеств каждого бренда
 
-# TODO: your code here
+number_big = sorted(number, reverse=True)[0] # наибольшее количество
+big = []
+
+for i in range(length):
+    if number[i] == number_big:
+        big.append(Brand_1[i])
+# big - список максимальных брендов
+
+print("На складе больше всего товаров брэнда(ов):", ', '.join(big))
+
+Price = []
+for i in range(len(items)):
+    Price.append(dict(items[i])['price'])  # все цены
+
+big_price = []
+for i in range(len(items)):
+    if Price[i] == max(Price):
+        num_max = i
+        big_price.append(Brand[i])    # наименования брэндов с самой высокой ценой
+
+print("На складе самый дорогой товар брэнда(ов):", ', '.join(big_price), f"- цена {max(Price)} рублей")
