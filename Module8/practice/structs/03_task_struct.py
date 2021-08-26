19cef2b87030a2f6a35db1ccc10b5badfd54dac1
LyubovBabushkina <89097675+LyubovBabushkina@users.noreply.github.com>
2021-08-26 20:22:52 +0300

    Update 03_task_struct.py

--- Module8/practice/structs/03_task_struct.py
+++ Module8/practice/structs/03_task_struct.py
@@ -4,3 +4,20 @@
 # 1. Количество элементов списка не превышающие 10
 # 2. Сумму всех положительных элементов списка
 # 3. Среднее арифметическое всех четных элементов
+
+lst = [4, -7, 10, 21, -100, 44, 57, -4, 0]
+
+nomore_ten = [el for el in lst if el <= 10]
+print(len(nomore_ten))
+
+positive = [el for el in lst if el > 0]
+sum_positive = 0
+for el in positive:
+    sum_positive += el
+print(sum_positive)
+
+even = [el for el in lst if el % 2 == 0]
+sum = 0
+for el in even:
+    sum += el
+print(sum / len(even))
