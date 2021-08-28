eab55aba901949bc175c5e6ed1c8af8f55b271a5
LyubovBabushkina <89097675+LyubovBabushkina@users.noreply.github.com>
2021-08-29 00:54:39 +0300

    Update 10_task_result.py

--- Module10/practice/result/10_task_result.py
+++ Module10/practice/result/10_task_result.py
@@ -3,3 +3,18 @@
 # Примечание: все элементы списка должны быть гарантировано уникальными(неповторяющимися)
 # Если создать список с заданными параметрами невозможно - функция должна выбросить исключение(любое)
 
+import random
+def random_unique(n, a, b):
+    try:
+        if 0 < n <= b - a + 1:
+            numbers = []
+            while len(numbers) < n:
+                new = random.randint (a, b)
+                if numbers.count(new) == 0:
+                    numbers.append(new)
+        return numbers
+    except UnboundLocalError:
+        return print("Некорректное число")
+
+print(random_unique(7, -2, 10))
+print(random_unique(17, -2, 10))
