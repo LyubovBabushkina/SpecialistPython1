8bbce4f111e8d3c9ce2092b08552d437e4412526
LyubovBabushkina <89097675+LyubovBabushkina@users.noreply.github.com>
2021-08-29 00:31:34 +0300

    Update 09_task_result.py

--- Module10/practice/result/09_task_result.py
+++ Module10/practice/result/09_task_result.py
@@ -1,3 +1,10 @@
 # Напишите функцию принимающую на вход целое числом n. И возвращающую список из n элементов.
 # заполненный случайными целыми числами в диапазоне от a до b.
 # Примечание: для генерации случайного числа используйте import random
+
+import random
+def random_numbers(n, a, b):
+    numbers = []
+    for _ in range(n):
+        numbers.append(random.randint (a, b))
+    return numbers
