3862d363c5ac787ab5142a626b64f739cd36e654
LyubovBabushkina <89097675+LyubovBabushkina@users.noreply.github.com>
2021-08-29 01:31:54 +0300

    Update 07_task_result.py

--- Module10/practice/result/07_task_result.py
+++ Module10/practice/result/07_task_result.py
@@ -1,1 +1,10 @@
 # Напишите программу генерирующую и записывающую в файле произвольное 20000-значное целое число.
+def big_number():
+    import random
+    f = open("twenty_thousand_digits.txt", "w", encoding="utf-8")
+    for _ in range(20000):
+        a = str(random.randint(0, 9))
+        f.write(a)
+    f.close()
+
+big_number()
